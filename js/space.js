9d1d062b8e7662b1198a49ed571a99e3414dd8cc
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-09-06 19:48:42 -0300

    Solución

--- js/space.js
+++ js/space.js
@@ -1,15 +1,48 @@
+document.addEventListener("DOMContentLoaded", function(){
+
 const btnBuscar = document.getElementById("btnBuscar");
 let inputBuscar = document.getElementById("inputBuscar");
 const URL_NASA = "https://images-api.nasa.gov/search?q=";
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
+let item = [];
 
-function mostrarResultados(datos){
-    let imgTemp = document.createElement("img");
-    imgTemp.src = datos.collection.items[0].data[0].href
-    imgTemp.width = "300px";
-    imgTemp.height = "300px";
-    container.appendChild(imgTemp);
+function createText(options) {
+    const element = document.createElement(options.element);
+    element.classList.add(options.class);
+    element.textContent = options.text;
+
+    return element;
 }
+  
+// crea un elemento IMG y le agrega atributos pasados por parámetro (class, src y alt) 
+function createImage(options) {
+    const imageElement = document.createElement("img");
+    imageElement.classList.add(options.class);
+    imageElement.src = options.image;
+    imageElement.alt = options.name;
+
+    return imageElement;
+}
+
+function displayItem(item) {
+    // Crea el contenedor del producto
+    const itemElement = document.createElement("div");
+    itemElement.className = "item";
+  
+    // crea elementos del contenedor (imagen, nombre, descripción, precio y cantidad de vendidos)
+    const contentList = [
+      createImage({ class: 'imageElement', image: item.links[0].href }),
+      createText({ element: 'h2', class: 'nameElement', text: item.data[0].title }),
+      createText({ element: 'p', class: 'descriptionElement', text: item.data[0].description }),
+      createText({ element: 'p', class: 'dateElement', text: item.data[0].date_created }),
+    ];
+  
+    // agrega cada uno de los elementos al contenedor
+    contentList.forEach(item => itemElement.appendChild(item));
+    // agrega el contenedor a la lista de elementos
+    const container = document.getElementById("contenedor");
+    container.appendChild(itemElement);
+  }
 
 btnBuscar.addEventListener("click", function(){
     let nombre = inputBuscar.value;
@@ -19,9 +52,12 @@ btnBuscar.addEventListener("click", function(){
         return response.json()
     })
     .then((data) => {
-        mostrarResultados(data);
+        item = data.collection.items;
+        item.forEach(objeto => displayItem(objeto));
     })
     .catch((error) => {
         console.log(error)
     })
 })
+
+});
