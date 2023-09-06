61a7e7a5738e8b01aaf57e20e99152b707e3a89d
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-09-06 20:01:00 -0300

    Versión final

--- js/space.js
+++ js/space.js
@@ -1,63 +1,68 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-const btnBuscar = document.getElementById("btnBuscar");
-let inputBuscar = document.getElementById("inputBuscar");
-const URL_NASA = "https://images-api.nasa.gov/search?q=";
-// const container = document.getElementById("container");
-let item = [];
+    const btnBuscar = document.getElementById("btnBuscar");
+    let inputBuscar = document.getElementById("inputBuscar");
+    const URL_NASA = "https://images-api.nasa.gov/search?q=";
+    let contenedor = document.getElementById("contenedor");
+    let item = [];
 
-function createText(options) {
-    const element = document.createElement(options.element);
-    element.classList.add(options.class);
-    element.textContent = options.text;
+    function createText(options) {
+        const element = document.createElement(options.element);
+        element.classList.add(options.class);
+        element.textContent = options.text;
 
-    return element;
-}
-  
-// crea un elemento IMG y le agrega atributos pasados por parámetro (class, src y alt) 
-function createImage(options) {
-    const imageElement = document.createElement("img");
-    imageElement.classList.add(options.class);
-    imageElement.src = options.image;
-    imageElement.alt = options.name;
+        return element;
+    }
+    
+    function createImage(options) {
+        const imageElement = document.createElement("img");
+        imageElement.classList.add(options.class);
+        imageElement.src = options.image;
+        imageElement.alt = options.name;
 
-    return imageElement;
-}
+        return imageElement;
+    }
 
-function displayItem(item) {
-    // Crea el contenedor del producto
-    const itemElement = document.createElement("div");
-    itemElement.className = "item";
-  
-    // crea elementos del contenedor (imagen, nombre, descripción, precio y cantidad de vendidos)
-    const contentList = [
-      createImage({ class: 'imageElement', image: item.links[0].href }),
-      createText({ element: 'h2', class: 'nameElement', text: item.data[0].title }),
-      createText({ element: 'p', class: 'descriptionElement', text: item.data[0].description }),
-      createText({ element: 'p', class: 'dateElement', text: item.data[0].date_created }),
-    ];
-  
-    // agrega cada uno de los elementos al contenedor
-    contentList.forEach(item => itemElement.appendChild(item));
-    // agrega el contenedor a la lista de elementos
-    const container = document.getElementById("contenedor");
-    container.appendChild(itemElement);
-  }
+    function displayItem(item) {
+        const itemElement = document.createElement("div");
+        itemElement.className = "item";
+    
+        const contentList = [
+        createImage({ class: 'imageElement', image: item.links[0].href }),
+        createText({ element: 'h2', class: 'nameElement', text: item.data[0].title }),
+        createText({ element: 'p', class: 'descriptionElement', text: item.data[0].description }),
+        createText({ element: 'p', class: 'dateElement', text: item.data[0].date_created }),
+        ];
+    
 
-btnBuscar.addEventListener("click", function(){
-    let nombre = inputBuscar.value;
-    console.log(URL_NASA + nombre);
-    fetch(URL_NASA + nombre)
-    .then((response) => {
-        return response.json()
+        contentList.forEach(item => itemElement.appendChild(item));
+        
+        const container = document.getElementById("contenedor");
+        container.appendChild(itemElement);
+    }
+
+    function borrarLista(){
+        contenedor.innerHTML = "";
+    }
+
+    btnBuscar.addEventListener("click", function(){
+        let nombre = inputBuscar.value;
+        console.log(URL_NASA + nombre);
+        fetch(URL_NASA + nombre)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            item = data.collection.items;
+            item.forEach(objeto => displayItem(objeto));
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     })
-    .then((data) => {
-        item = data.collection.items;
-        item.forEach(objeto => displayItem(objeto));
+
+    btnBorrar.addEventListener("click", function(){
+        borrarLista();
     })
-    .catch((error) => {
-        console.log(error)
-    })
-})
 
 });
